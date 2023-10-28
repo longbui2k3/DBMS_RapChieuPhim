b079075148a910282745189ea93cfc8d2c9ef157
loctp2003 <loctp2003@gmail.com>
2023-10-28 12:37:02 +0700

    loc

--- UC/PhimUC.Designer.cs
+++ UC/PhimUC.Designer.cs
@@ -28,24 +28,354 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_Maphim = new System.Windows.Forms.TextBox();
+            this.txt_Tenphim = new System.Windows.Forms.TextBox();
+            this.txt_Daodien = new System.Windows.Forms.TextBox();
+            this.txt_Thoiluong = new System.Windows.Forms.TextBox();
+            this.txt_Dienvienchinh = new System.Windows.Forms.TextBox();
+            this.txt_Dotuoi = new System.Windows.Forms.TextBox();
+            this.txt_Quocgia = new System.Windows.Forms.TextBox();
+            this.txt_Nhasanxuat = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtTomtat = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Xem = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.picFilm = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.picFilm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(57, 39);
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Phim";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã phim";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên phim";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(883, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Thời lượng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Ngày khởi chiếu";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Đạo diễn";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(335, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Quốc gia";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(335, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Diễn viên chính";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(335, 135);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 16);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Nhà sản xuất";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(662, 24);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 16);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Tóm tắt phim";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(335, 102);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 16);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Độ tuổi";
+            // 
+            // txt_Maphim
+            // 
+            this.txt_Maphim.Location = new System.Drawing.Point(144, 27);
+            this.txt_Maphim.Name = "txt_Maphim";
+            this.txt_Maphim.Size = new System.Drawing.Size(158, 22);
+            this.txt_Maphim.TabIndex = 12;
+            this.txt_Maphim.TextChanged += new System.EventHandler(this.txt_Maphim_TextChanged);
+            // 
+            // txt_Tenphim
+            // 
+            this.txt_Tenphim.Location = new System.Drawing.Point(144, 62);
+            this.txt_Tenphim.Name = "txt_Tenphim";
+            this.txt_Tenphim.Size = new System.Drawing.Size(158, 22);
+            this.txt_Tenphim.TabIndex = 13;
+            // 
+            // txt_Daodien
+            // 
+            this.txt_Daodien.Location = new System.Drawing.Point(144, 96);
+            this.txt_Daodien.Name = "txt_Daodien";
+            this.txt_Daodien.Size = new System.Drawing.Size(158, 22);
+            this.txt_Daodien.TabIndex = 14;
+            // 
+            // txt_Thoiluong
+            // 
+            this.txt_Thoiluong.Location = new System.Drawing.Point(144, 132);
+            this.txt_Thoiluong.Name = "txt_Thoiluong";
+            this.txt_Thoiluong.Size = new System.Drawing.Size(158, 22);
+            this.txt_Thoiluong.TabIndex = 15;
+            // 
+            // txt_Dienvienchinh
+            // 
+            this.txt_Dienvienchinh.Location = new System.Drawing.Point(444, 62);
+            this.txt_Dienvienchinh.Name = "txt_Dienvienchinh";
+            this.txt_Dienvienchinh.Size = new System.Drawing.Size(161, 22);
+            this.txt_Dienvienchinh.TabIndex = 16;
+            // 
+            // txt_Dotuoi
+            // 
+            this.txt_Dotuoi.Location = new System.Drawing.Point(444, 99);
+            this.txt_Dotuoi.Name = "txt_Dotuoi";
+            this.txt_Dotuoi.Size = new System.Drawing.Size(161, 22);
+            this.txt_Dotuoi.TabIndex = 17;
+            // 
+            // txt_Quocgia
+            // 
+            this.txt_Quocgia.Location = new System.Drawing.Point(444, 24);
+            this.txt_Quocgia.Name = "txt_Quocgia";
+            this.txt_Quocgia.Size = new System.Drawing.Size(161, 22);
+            this.txt_Quocgia.TabIndex = 18;
+            // 
+            // txt_Nhasanxuat
+            // 
+            this.txt_Nhasanxuat.Location = new System.Drawing.Point(444, 132);
+            this.txt_Nhasanxuat.Name = "txt_Nhasanxuat";
+            this.txt_Nhasanxuat.Size = new System.Drawing.Size(161, 22);
+            this.txt_Nhasanxuat.TabIndex = 19;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(151, 170);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // txtTomtat
+            // 
+            this.txtTomtat.Location = new System.Drawing.Point(665, 50);
+            this.txtTomtat.Name = "txtTomtat";
+            this.txtTomtat.Size = new System.Drawing.Size(242, 171);
+            this.txtTomtat.TabIndex = 22;
+            this.txtTomtat.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Location = new System.Drawing.Point(45, 204);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(75, 44);
+            this.btn_Them.TabIndex = 24;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Location = new System.Drawing.Point(151, 204);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(75, 44);
+            this.btn_Xoa.TabIndex = 25;
+            this.btn_Xoa.Text = "Xóa";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Location = new System.Drawing.Point(261, 204);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(75, 44);
+            this.btn_Sua.TabIndex = 26;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btn_Xem
+            // 
+            this.btn_Xem.Location = new System.Drawing.Point(368, 204);
+            this.btn_Xem.Name = "btn_Xem";
+            this.btn_Xem.Size = new System.Drawing.Size(75, 44);
+            this.btn_Xem.TabIndex = 27;
+            this.btn_Xem.Text = "Xem";
+            this.btn_Xem.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(956, 188);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(108, 33);
+            this.btn.TabIndex = 28;
+            this.btn.Text = "Xem";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picFilm
+            // 
+            this.picFilm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picFilm.Location = new System.Drawing.Point(943, 24);
+            this.picFilm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picFilm.Name = "picFilm";
+            this.picFilm.Size = new System.Drawing.Size(138, 145);
+            this.picFilm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFilm.TabIndex = 56;
+            this.picFilm.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 254);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1084, 258);
+            this.dataGridView1.TabIndex = 57;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(196, 464);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(8, 8);
+            this.dataGridView2.TabIndex = 58;
             // 
             // PhimUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.picFilm);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btn_Xem);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.btn_Them);
+            this.Controls.Add(this.txtTomtat);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txt_Nhasanxuat);
+            this.Controls.Add(this.txt_Quocgia);
+            this.Controls.Add(this.txt_Dotuoi);
+            this.Controls.Add(this.txt_Dienvienchinh);
+            this.Controls.Add(this.txt_Thoiluong);
+            this.Controls.Add(this.txt_Daodien);
+            this.Controls.Add(this.txt_Tenphim);
+            this.Controls.Add(this.txt_Maphim);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "PhimUC";
             this.Size = new System.Drawing.Size(1120, 591);
+            ((System.ComponentModel.ISupportInitialize)(this.picFilm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,5 +384,36 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_Maphim;
+        private System.Windows.Forms.TextBox txt_Tenphim;
+        private System.Windows.Forms.TextBox txt_Daodien;
+        private System.Windows.Forms.TextBox txt_Thoiluong;
+        private System.Windows.Forms.TextBox txt_Dienvienchinh;
+        private System.Windows.Forms.TextBox txt_Dotuoi;
+        private System.Windows.Forms.TextBox txt_Quocgia;
+        private System.Windows.Forms.TextBox txt_Nhasanxuat;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RichTextBox txtTomtat;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Xem;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.PictureBox picFilm;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- UC/PhimUC.cs
+++ UC/PhimUC.cs
@@ -16,5 +16,40 @@ namespace QuanLyRapChieuPhim.UC
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Maphim_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
